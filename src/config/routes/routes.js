71e0c84894a40f2ccfdfe408a041054f940b7391
gabriel-roque <gabrielrms.dll@gmail.com>
2020-06-10 14:37:59 -0300

    refactor(routes): reorganize order declaration endpoint resource pokemon

--- src/config/routes/routes.js
+++ src/config/routes/routes.js
@@ -8,8 +8,8 @@ const PokemonController = require('../../app/controllers/pokemon.controller');
 routes.get('/', RootController.index);
 
 routes.get('/pokemons', PokemonController.index);
-routes.get('/pokemons/:id', PokemonController.show);
 routes.post('/pokemons', PokemonController.store);
+routes.get('/pokemons/:id', PokemonController.show);
 routes.put('/pokemons/:id', PokemonController.update);
 routes.delete('/pokemons/:id', PokemonController.delete);
 
