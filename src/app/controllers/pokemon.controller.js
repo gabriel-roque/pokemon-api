6702d6dceb01ac1dac25a2eb5232b9a7d74575e0
gabriel-roque <gabrielrms.dll@gmail.com>
2020-06-10 14:36:41 -0300

    feat(pokemon): add operation update, delete and validated operation

--- src/app/controllers/pokemon.controller.js
+++ src/app/controllers/pokemon.controller.js
@@ -3,14 +3,56 @@ const { validateRequest } = require('../services/pokemon.services');
 
 module.exports = {
   async index(request, response) {
-    const pokemons = await Pokemon.findAll();
+    const pokemons = await Pokemon.findAll({
+      order: [['id', 'ASC']],
+    });
 
     return response.status(200).json(pokemons);
   },
 
-  async store(request, response) {
-    const result = await validateRequest(request.body);
+  async show(request, response) {
+    const { id } = request.params;
 
-    return response.status(result.status).json(result.response);
+    try {
+      const pokemon = await Pokemon.findByPk(id);
+      if (pokemon === null) throw notFound;
+      return response.status(200).json(pokemon);
+    } catch (e) {
+      return response.status(404).json({ message: 'Pokemon not found' });
+    }
+  },
+
+  async store(request, response) {
+    try {
+      const result = await validateRequest(request.body);
+
+      return response.status(result.status).json(result.response);
+    } catch (e) {
+      return response.status(400).json({ message: 'Failed in create pokemon' });
+    }
+  },
+
+  async update(request, response) {
+    const { treinador } = request.body;
+
+    try {
+      const pokemon = await Pokemon.findByPk(request.params.id);
+      await pokemon.update({ treinador });
+
+      return response.status(204).json();
+    } catch (e) {
+      return response.status(404).json({ message: 'Pokemon not found' });
+    }
+  },
+
+  async delete(request, response) {
+    try {
+      const pokemon = await Pokemon.findByPk(request.params.id);
+      await pokemon.destroy();
+
+      return response.status(204).json();
+    } catch (e) {
+      return response.status(404).json({ message: 'Pokemon not found' });
+    }
   },
 };
--- src/config/routes/routes.js
+++ src/config/routes/routes.js
@@ -8,6 +8,9 @@ const PokemonController = require('../../app/controllers/pokemon.controller');
 routes.get('/', RootController.index);
 
 routes.get('/pokemons', PokemonController.index);
+routes.get('/pokemons/:id', PokemonController.show);
 routes.post('/pokemons', PokemonController.store);
+routes.put('/pokemons/:id', PokemonController.update);
+routes.delete('/pokemons/:id', PokemonController.delete);
 
 module.exports = routes;
